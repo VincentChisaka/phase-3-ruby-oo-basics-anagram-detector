644dd6e0130871ba731113e6bebbd1690f6b623d
VincentChisaka <vincentchisaka2@gmail.com>
2023-02-10 21:48:05 +0300

    done lab

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :word
+  
+    def initialize (word)
+      @word = word
+    end
+  
+    def match array
+      matches = []
+      array.each do |item|
+        if item.chars.sort == @word.chars.sort
+          matches << item
+        end
+      end
+      return matches    
+    end  
+  end  
+  
+  listen = Anagram.new("rescue")
+  puts listen.match(%w[enlists secure google inlets banana])
+  
